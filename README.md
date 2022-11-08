f075d008f2c5f41050fe2cda08808c47691da06b
Leonardo <leopesi@yahoo.com.br>
2022-11-08 14:28:35 -0300

    Refactoring folders

--- README.md
+++
@@ -1,1 +0,0 @@
-# Proyecto final del curso Programador Web Avanzado da UTN - (Universidad Tecnológica Nacional - Buenos Aires)
--- src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userRoutes = require('./usuario.routes');
-const authRoutes = require('./auth.routes')
-
-router.use('/', userRoutes, authRoutes);
-
-module.exports = router;
