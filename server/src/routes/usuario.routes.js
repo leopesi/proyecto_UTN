5a03592744f5cd9d860b1f41a32c8dd0d14dd397
Leonardo <leopesi@yahoo.com.br>
2022-11-23 19:22:55 -0300

    refact usuario.routes.js

--- server/src/routes/usuario.routes.js
+++ server/src/routes/usuario.routes.js
@@ -1,64 +1,17 @@
-require('dotenv').config()
-const routes = require('express').Router();
-const jwt = require('jsonwebtoken');
 const userController = require('../controllers/usuario.controller');
-
-//Credenciais
-const SECRET = process.env.SECRET
-const API_URL = "http://localhost:8080/api/test/";
+const API_URL = "/api/test/";
 
 module.exports = function(app) {
     app.use(function(req, res, next) {
-      res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-      );
+        res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
       next();
     });
   
-    app.get(API_URL + "all", 
-    controller.allAccess);
+    app.get(API_URL + "all", userController.allAccess);
   
-    app.get(API_URL + "user",
-      [authJwt.verifyToken],
-      controller.userBoard
-    );
+    //app.get(API_URL + "user", [authJwt.verifyToken], userController.userBoard);
   
-    app.get(API_URL + "mod",
-      [authJwt.verifyToken, 
-      authJwt.isModerator],
-      controller.moderatorBoard
-    );
+    //app.get(API_URL + "mod", [authJwt.verifyToken, authJwt.isModerator], userController.moderatorBoard);
   
-    app.get(API_URL + "admin",
-      [authJwt.verifyToken, 
-      authJwt.isAdmin],
-      controller.adminBoard
-    );
+    //app.get(API_URL + "admin", [authJwt.verifyToken, authJwt.isAdmin], userController.adminBoard);
   };
-/*
-function checkToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-
-    if (!token) {
-        return res.status(401).json({msg: 'Acceso no permitido!'})
-    }
-    try {
-        jwt.verify(token, SECRET)
-        next()
-    } catch(error){
-        res.status(400).json({msg: "Token Inválido"})
-    }
-}
-
-
-//Metodo de peticion
-routes.post('/user/search', userController.search);
-routes.get('/user', userController.find);
-routes.get('/user/:id', userController.findById);
-routes.put('/user/:id', userController.update);
-routes.delete('/user/:id', checkToken, userController.deleteById);
-
-//Modulo
-module.exports = routes;*/
