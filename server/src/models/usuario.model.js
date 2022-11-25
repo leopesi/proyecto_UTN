85c77c60a331e29d13ba4d358c33e4d5050c76f8
Leonardo <leopesi@yahoo.com.br>
2022-11-25 17:56:12 -0300

    refac usuario.model.js

--- server/src/models/usuario.model.js
+++ server/src/models/usuario.model.js
@@ -1,38 +1,27 @@
-const db = require("../db/db");
-
-const Sequelize = require('sequelize');
-
-const User = db.define('users4', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    nombre: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    apellido: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-
-
-})
-//Criar a tabela caso ainda não esteja criada
-//User.sync();
-
-
-//Verifica e faz alteração na tabela caso exista alguma
-User.sync({alter: true})
-
-module.exports = User;
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define('users4', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        nombre: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        apellido: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    });
+    return User
+}
