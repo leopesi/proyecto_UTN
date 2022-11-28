837712786db6569bc6249ce4fb24e76a8c8459df
Leonardo <leopesi@yahoo.com.br>
2022-11-28 10:50:04 -0300

    added store.js

---
+++ client/store.js
@@ -0,0 +1,15 @@
+/* In the reducers/index.js, we used combineReducers() to combine 2 reducers into one. Let’s import it, and pass it to createStore(): */
+
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
