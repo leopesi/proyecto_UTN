f743b28f3b9759a500455a29d283d9d01577aced
Leonardo <leopesi@yahoo.com.br>
2023-01-06 15:49:44 -0300

    Update to deploy3

--- server/src/config.js
+++ server/src/config.js
@@ -4,7 +4,6 @@ module.exports = Object.freeze({
   DB_USER: process.env.MYSQLUSER || "root",
   DB_PASS: process.env.MYSQLPASSWORD || "Z0rZaCSyy0JgoIaaH7W9",
   DB_DATABASE:  process.env.MYSQLDATABASE || "railway",
-  
 }) 
 
 
