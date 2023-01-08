ab183b6e85451a701bf5853a7aa5b02dc0572e70
Leonardo <leopesi@yahoo.com.br>
2023-01-08 20:05:16 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
